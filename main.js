50bf877fe76d44c6aadc26af0a7ceab2f1b89a5f
Renan Rocha <renan.aragorn@gmail.com>
2022-05-05 16:30:18 -0300

    expanded menu bug fixes and updated responsiveness

--- main.js
+++ main.js
@@ -5,3 +5,19 @@ function onScroll(){
     navigation.classList.add('scroll')
   }
 }
+
+function openMenu() {
+  const buttonOpenMenu = document.querySelector(".open-menu")
+  buttonOpenMenu.addEventListener('click', function() {
+    document.body.classList.add('menu-expanded')
+  })
+}
+
+function closeMenu() {
+  const buttonCloseMenu = document.querySelector(".close-menu")
+  buttonCloseMenu.addEventListener('click', function() {
+    document.body.classList.remove('menu-expanded')
+  })
+}
+openMenu()
+closeMenu()
