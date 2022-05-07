a1f9bca6168e68df033b818531b95fec43d42774
Renan Rocha <renan.aragorn@gmail.com>
2022-05-07 11:56:06 -0300

    animation and sticked button added

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 function onScroll(){
-  if (scrollY == 0) {
-    navigation.classList.remove('scroll')
-  } else {
+  if (scrollY > 0) {
     navigation.classList.add('scroll')
+    stick.style.visibility = "visible"
+  } else {
+    navigation.classList.remove('scroll')
+    stick.style.visibility = "hidden"
   }
 }
 
@@ -18,3 +20,28 @@ function closeMenu() {
 }
 
 openMenu()
+
+ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700,
+  delay: 200
+}).reveal(`
+  #above-the-fold div,
+  #above-the-fold button,
+  #above-the-fold img,
+  #services,
+  #services .label,
+  #services .h2,
+  #services .card,
+  #about-us,
+  #about-us .label,
+  #about-us h2,
+  #about-us p,
+  #about-us img,
+  #contact-us,
+  #contact-us h2,
+  #contact-us .contact-info,
+  #contact-us button,
+  #contact-us img
+`);
